d3d42a9d1af4e1b985d59a8bdc5e59332b33b447
Max Maton <max@maton.info>
2015-03-21 20:02:43 +0100

    More connection code

--- ZeroKnowledge/Controllers/ConnectionController.cs
+++ ZeroKnowledge/Controllers/ConnectionController.cs
@@ -5,6 +5,8 @@ using System.Collections;
 using System.Linq;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Net.Sockets;
+using System.Net;
 
 namespace ZeroKnowledge
 {
@@ -15,6 +17,7 @@ namespace ZeroKnowledge
 			Dictionary<string, Program> programs = new Dictionary<string, Program> ();
 
 			var plist = Process.GetProcesses ();
+			var result = new List<Connection> ();
 
 			foreach (var p in plist) {
 				var uid = GetUser (p.Id);
@@ -26,29 +29,101 @@ namespace ZeroKnowledge
 					programs.Add (uid, new Program (){ Identifier = uid, ProcessId = p.Id });
 			}
 
-			foreach(var p in plist)
-				foreach (var parts in GetConnectionData(p.Id)) {
-					Debug.WriteLine (String.Join(" ", parts));
+			foreach (var p in plist) {
+				foreach (var con in ParseNetFile (String.Format("/proc/{0}/net/tcp", pid))) {
+					con.Type = "tcp";
+					con.Program = p;
+					result.Add (con);
 				}
 
-			return null;
+				foreach (var con in ParseNetFile (String.Format("/proc/{0}/net/tcp6", pid))) {
+					con.Type = "tcp";
+					con.Program = p;
+					result.Add (con);
+				}
+				foreach (var con in ParseNetFile(String.Format("/proc/{0}/net/udp", pid))){
+					con.Type = "udp";
+					con.Program = p;
+					result.Add (con);
+				}
+				foreach (var con in ParseNetFile(String.Format("/proc/{0}/net/udp6", pid))){
+					con.Type = "udp";
+					con.Program = p;
+					result.Add (con);
+				}
+			}
+
+			return result;
 		}
 
-		private static IEnumerable<string[]> GetConnectionData()
+		private static IEnumerable<Connection> ParseNetFile(string filename)
 		{
-			/*
-			ProcessStartInfo pinfo = new ProcessStartInfo("/system/bin/netstat");
-			pinfo.RedirectStandardOutput = true;
-			pinfo.UseShellExecute = false;
-			var proc = Process.Start(pinfo);
+			FileInfo f = new FileInfo (filename);
+			if (!f.Exists)
+				yield break;
 
-			proc.WaitForExit();
-			var lines = proc.StandardOutput;
-			while (!lines.EndOfStream) {
-				var line = lines.ReadLine ();
+			var reader = new StreamReader (f.OpenRead ());
+
+			while (!reader.EndOfStream) {
+				var line = reader.ReadLine ();
+				if (String.IsNullOrWhiteSpace (line))
+					continue;
 				var parts = Regex.Split (line, @"\s+");
-				yield return parts;
-			}*/
+
+				if (parts [1] == "sl")
+					continue;
+
+				var source = ParseHexEndpoint (parts [2]);
+				var dest = ParseHexEndpoint (parts [3]);
+				var packets = long.Parse (parts [5].Split(':')[1], System.Globalization.NumberStyles.HexNumber);
+
+				var c = new Connection ();
+				c.Source = source;
+				c.Destination = dest;
+				c.Activity = packets;
+				yield return c;
+			}
+		}
+
+		static IPEndPoint ParseHexEndpoint (string description)
+		{
+			var c = new Connection ();
+			var ippart = description.Split (':') [0];
+
+			if ((BitConverter.IsLittleEndian && ippart.Length != 8) ||
+				(!BitConverter.IsLittleEndian && ippart.Length == 8))
+			{
+				ippart = SwapHexEndianness (ippart);
+			}
+
+
+			IPAddress ip;
+			if (ippart.Length == 8)
+				ip = new IPAddress (uint.Parse (ippart, System.Globalization.NumberStyles.HexNumber));
+			else {
+				string representation = "";
+				for (int i = 0; i < 8; i++)
+					representation += ippart.Substring (i * 4, 4) + ":";
+				ip = IPAddress.Parse(representation.Substring(0, representation.Length - 1));
+			}
+
+			var sourceport = int.Parse (description.Split (':') [1], System.Globalization.NumberStyles.HexNumber);
+
+			return new IPEndPoint (ip, sourceport);
+		}
+
+		public static string SwapHexEndianness(string input)
+		{
+			if (input.Length % 2 != 0)
+				throw new ArgumentException ("input is not a hex string", "input");
+
+			string result = "";
+			for(int i = 2; i < input.Length + 1; i += 2)
+			{
+				result += input.Substring (input.Length - i, 2);
+			}
+
+			return result;
 		}
 
 		public static string GetUser(int pid)
@@ -58,7 +133,7 @@ namespace ZeroKnowledge
 				return null;
 
 			var reader = new StreamReader (f.OpenRead ());
-			var uid = reader.ReadLine ();//.Split ('/') [1];
+			var uid = reader.ReadLine ();
 
 			// Filter system processes
 			if (uid != null && uid.Contains ("/"))
--- ZeroKnowledge/Models/Connection.cs
+++ ZeroKnowledge/Models/Connection.cs
@@ -11,7 +11,14 @@ namespace ZeroKnowledge
 		public string Type { get; set; }
 		public DateTime ConnectionStart { get; set; }
 
+		public long Activity { get; set; }
+
 		public string HostName { get; set; }
+
+		public override string ToString ()
+		{
+			return string.Format ("[Connection: Program={0}, Source={1}, Destination={2}, Type={3}, ConnectionStart={4}, Activity={5}, HostName={6}]", Program, Source, Destination, Type, ConnectionStart, Activity, HostName);
+		}
 	}
 }
 
