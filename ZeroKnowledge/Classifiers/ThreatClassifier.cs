a0439ecdf06e8055bb38a2eb8a8fdce9d7519bfd
Jonathan Staats <jjstaats@gmail.com>
2015-03-22 01:12:04 +0100

    Added extra predictors.

--- ZeroKnowledge/Classifiers/ThreatClassifier.cs
+++ ZeroKnowledge/Classifiers/ThreatClassifier.cs
@@ -9,14 +9,29 @@ namespace ZeroKnowledge
 	{
 		List<Func<Connection, double>> _predictors = new List<Func<Connection, double>>();
 
-		public ThreatClassifier ()
-		{
-			_predictors.Add ((c) => 1);
-			_predictors.Add ((c) => c.HostName != null ? (c.HostName.Contains ("spotify") ? 0 : 1) : 1);
-			_predictors.Add ((c) => c.HostName == null ? 1 : 0);
+		public bool MatchHostNameBundleName(string host, Program program) {
+			if (host == null || program == null) {
+				return false;
+			}
+			var parts1 = host.Split ('.');
+			var parts2 = program.Identifier.Split ('.');
+
+			if (parts1.Length < 2 || parts2.Length < 2) {
+				return false;
+			}
+
+			return parts2 [1] == parts1 [parts1.Length - 2];
 		}
 
-		public double[] Weights = new double[]{0.1, 0.8, 0.1, 1.0};
+		public ThreatClassifier ()
+		{
+			_predictors.Add (c => 1);
+			_predictors.Add (c => MatchHostNameBundleName (c.HostName, c.Program) ? 0 : 1);
+			_predictors.Add (c => Math.Min (c.Activity, 20) / 20.0);
+			_predictors.Add (c => c.HostName == null ? 1 : 0);
+		}
+
+		public double[] Weights = {0.01, 0.8, 0.5, 0.1, 1.0};
 
 		public void Classify(List<Connection> connections) {
 
